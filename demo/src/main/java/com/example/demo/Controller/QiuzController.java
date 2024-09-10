d0b1fef4edbc7510dc51d05d5e039dfdc87e771a
三寺 <taku.mitera@rakus-partners.co.jp>
2024-09-10 11:36:35 +0900

    基本機能

---
+++ demo/src/main/java/com/example/demo/Controller/QiuzController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class QiuzController {
+    
+    @RequestMapping("")
+    public String main(){
+        return "HTML/main";
+    }
+    
+    
+}
